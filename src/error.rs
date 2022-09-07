43e8b0f9a14a126821b25bed13032e97b6e328f3
Blue Kirby <blue.kirby@tutanota.com>
2022-09-08 00:16:46 +0100

    feat: add wallet balance and tx fetch
    Signed-off-by: Blue Kirby <blue.kirby@tutanota.com>

--- src/error.rs
+++ src/error.rs
@@ -3,12 +3,15 @@ use thiserror::Error;
 
 #[derive(Debug, Error, Deserialize)]
 pub enum ArweaveError {
-    #[error("Error getting transaction info: {0}")]
-    TransactionInfoError(String),
-
     #[error("Error getting network info: {0}")]
     NetworkInfoError(String),
 
+    #[error("Error getting transaction info: {0}")]
+    TransactionInfoError(String),
+
     #[error("Unknown Error.")]
     UnknownError,
+
+    #[error("Error getting wallet: {0}")]
+    WalletError(String),
 }
--- src/wallet/balance.rs
+++ src/wallet/balance.rs
@@ -1,10 +1,88 @@
-use pretend::{pretend, Result};
+use pretend::{pretend, resolver::UrlResolver, Pretend, Url};
+use pretend_reqwest::Client as HttpClient;
+
+use crate::error::ArweaveError;
 
 #[pretend]
 trait TransactionInfoFetch {
     #[request(method = "GET", path = "/wallet/{address}/balance")]
-    async fn wallet_balance(&self, address: &str) -> Result<String>;
+    async fn wallet_balance(&self, address: &str) -> pretend::Result<String>;
 
     #[request(method = "GET", path = "/wallet/{address}/last_tx")]
-    async fn wallet_last_tx_id(&self, address: &str) -> Result<String>;
+    async fn wallet_last_tx_id(&self, address: &str) -> pretend::Result<String>;
+}
+
+pub struct WalletInfoClient(Pretend<HttpClient, UrlResolver>);
+
+impl WalletInfoClient {
+    pub fn new(url: Url) -> Self {
+        let client = HttpClient::default();
+        let pretend = Pretend::for_client(client).with_url(url);
+        Self(pretend)
+    }
+
+    pub async fn balance(&self, address: &str) -> Result<String, ArweaveError> {
+        self.0
+            .wallet_balance(address)
+            .await
+            .map_err(|op| ArweaveError::WalletError(op.to_string()))
+    }
+
+    pub async fn last_tx_id(&self, address: &str) -> Result<String, ArweaveError> {
+        self.0
+            .wallet_last_tx_id(address)
+            .await
+            .map_err(|op| ArweaveError::WalletError(op.to_string()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use httpmock::{Method::GET, MockServer};
+    use pretend::Url;
+    use tokio_test::block_on;
+
+    use crate::wallet::balance::WalletInfoClient;
+
+    #[test]
+    fn test_balance() {
+        let address = "address";
+        let server = MockServer::start();
+        let server_url = server.url("");
+        let mock = server.mock(|when, then| {
+            when.method(GET)
+                .path(format!("/wallet/{}/balance", address));
+            then.status(200)
+                .header("Content-Type", "application/json")
+                .body("123123");
+        });
+
+        let url = Url::parse(&server_url).unwrap();
+        let client = WalletInfoClient::new(url);
+        let tx_info = block_on(client.balance(address)).unwrap();
+
+        mock.assert();
+        assert_eq!(tx_info, "123123".to_string());
+    }
+
+    #[test]
+    fn test_last_tx() {
+        let address = "address";
+        let server = MockServer::start();
+        let server_url = server.url("");
+        let mock = server.mock(|when, then| {
+            when.method(GET)
+                .path(format!("/wallet/{}/last_tx", address));
+            then.status(200)
+                .header("Content-Type", "application/json")
+                .body("last_tx");
+        });
+
+        let url = Url::parse(&server_url).unwrap();
+        let client = WalletInfoClient::new(url);
+        let tx_info = block_on(client.last_tx_id(address)).unwrap();
+
+        mock.assert();
+        assert_eq!(tx_info, "last_tx".to_string());
+    }
 }
