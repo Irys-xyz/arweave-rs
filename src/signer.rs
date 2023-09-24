96d9465eedb7c0f0c4281a8d7e96af245a875028
Miko Nieminen <miko.nieminen@iki.fi>
2023-09-24 21:23:42 +0200

    Fix issues pointed out by clippy

--- src/signer.rs
+++ src/signer.rs
@@ -68,7 +68,7 @@ impl ArweaveSigner {
 
         let pub_key = RsaPublicKey::from_public_key_der(jwk.key.to_der().as_slice()).unwrap();
         let mut hasher = sha2::Sha256::new();
-        hasher.update(&message);
+        hasher.update(message);
         let hashed = &hasher.finalize();
 
         let rng = thread_rng();
